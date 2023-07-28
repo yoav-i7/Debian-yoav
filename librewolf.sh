f2d7323c83f42cc0a4416200dd6e23ecc75679cd
Yoav <95529987+yoav-i7@users.noreply.github.com>
2023-07-28 17:59:37 +0300

    Create librewolf.sh
    A script to install librewolf

---
+++ librewolf.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Check if Script is Run as Root
+if [[ $EUID -ne 0 ]]; then
+  echo "You must be a root user to run this script, please run sudo ./install.sh" 2>&1
+  exit 1
+fi
+
+username=$(id -u -n 1000)
+builddir=$(pwd)
+
+# Updating the system
+apt update
+
+# Installing librewolf
+nala install -y wget gnupg lsb-release apt-transport-https ca-certificates
+
+distro=$(if echo " una bookworm vanessa focal jammy bullseye vera uma " | grep -q " $(lsb_release -sc) "; then echo $(lsb_release -sc); else echo focal; fi)
+
+sudo -u $username wget -O- https://deb.librewolf.net/keyring.gpg | gpg --dearmor -o /usr/share/keyrings/librewolf.gpg
+tee /etc/apt/sources.list.d/librewolf.sources << EOF > /dev/null
+Types: deb
+URIs: https://deb.librewolf.net
+Suites: $distro
+Components: main
+Architectures: amd64
+Signed-By: /usr/share/keyrings/librewolf.gpg
+EOF
+
+nala update
+nala install librewolf -y
+
